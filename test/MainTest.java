9948079a0cb6ffac350e4c3ad805e02d3e52d864
Jalen King <jkin2520@students.vsu.edu>
2024-12-08 11:26:52 -0500

    Add unit test for getEvenNumbers function

---
+++ test/MainTest.java
@@ -0,0 +1,12 @@
+import org.junit.Test;
+import static org.junit.Assert.*;
+import java.util.List;
+
+public class MainTest {
+    @Test
+    public void testGetEvenNumbers() {
+        List<Integer> input = List.of(1, 2, 3, 4, 5, 6);
+        List<Integer> expected = List.of(2, 4, 6);
+        assertEquals(expected, Main.getEvenNumbers(input));
+    }
+}
